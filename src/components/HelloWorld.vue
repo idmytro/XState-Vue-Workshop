8fbf2ef5bb675ecae8a993e0463cdc3ede2652ab
Dmytro <dmytro.cisar@gmail.com>
2023-04-16 12:34:22 +0300

    feat: add hooks

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,20 +1,20 @@
 <script setup>
-import {ref} from 'vue';
+import {useState} from 'vue-hooks-api';
 
 defineProps({
 	msg: String,
 });
 
-const count = ref(0);
+const [count, setCount] = useState(0);
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" @click="setCount(count++)">count is {{ count }}</button>
     <p>
-      Edit
+      !Edit
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
