6cf9aa4e4f0b5932ee7c4e014263f5319c67c912
Dmytro <dmytro.cisar@gmail.com>
2023-04-18 19:13:54 +0300

    style: eslint and fixes

--- src/components/ProgressCircle.vue
+++ src/components/ProgressCircle.vue
@@ -1,12 +1,23 @@
 <template>
-  <svg
-    viewBox="0 0 100 100"
-    width="100"
-    height="100"
-    fill="none"
-    class="circles"
-  >
-    <circle r="40" cx="50" cy="50" pathLength="1" />
-    <circle class="progress" r="40" cx="50" cy="50" pathLength="1" />
-  </svg>
+	<svg
+		viewBox="0 0 100 100"
+		width="100"
+		height="100"
+		fill="none"
+		class="circles"
+	>
+		<circle
+			r="40"
+			cx="50"
+			cy="50"
+			pathLength="1"
+		/>
+		<circle
+			class="progress"
+			r="40"
+			cx="50"
+			cy="50"
+			pathLength="1"
+		/>
+	</svg>
 </template>
